34ed39a3a2901c2f7aa8f85bf6e5f1075e5e5e45
Irmo manie <manie@catfp.ch>
2021-12-02 17:26:32 +0100

    Quick and dirty solution

--- src/main/scala/Day02.scala
+++ src/main/scala/Day02.scala
@@ -1,50 +1,40 @@
 import scala.io.Source
 
 object Day02 extends App {
-  sealed trait Direction
-  object Direction {
-    case object Up extends Direction
-    case object Down extends Direction
-    case object Forward extends Direction
-    case object Backward extends Direction
-  }
-  case class Movement(direction: Direction, steps: Int)
+  case class Movement(direction: String, steps: Int)
+  case class NormalPosition(x: Int, y: Int)
+  case class AimedPosition(x: Int, y: Int, a: Int)
 
   val source = Source.fromFile("input/day02.input")
   val movements = source.getLines().map(inputToMovement).toList
 
-  val calculatedPosition = movements.foldLeft((0,0)) { case ((x, y), movement) =>
-    movement match {
-      case Movement(Direction.Down, num) => (x, y + num)
-      case Movement(Direction.Up, num) => (x, y - num)
-      case Movement(Direction.Forward, num) => (x + num, y)
-      case Movement(Direction.Backward, num) => (x - num, y)
-    }
-  }
+  val calculatedPosition = movements.foldLeft(NormalPosition(0, 0)){ case (p, m) => calculateNextNormalPosition(p, m) }
 
   println(s"Position: ${calculatedPosition}")
-  var result = calculatedPosition._1 * calculatedPosition._2
-  println(s"Result: ${result}")
+  println(s"Result: ${calculatedPosition.x * calculatedPosition.y}")
 
-  val aimedCalculatedPosition = movements.foldLeft((0,0,0)) { case ((x, y, a), movement) =>
+  val aimedCalculatedPosition = movements.foldLeft(AimedPosition(0, 0, 0)){ case (p, m) => calculateNextAimedPosition(p, m) }
+  println(s"Aimed Position: ${aimedCalculatedPosition}")
+  println(s"Aimed Result: ${aimedCalculatedPosition.x * aimedCalculatedPosition.y}")
+
+  def calculateNextNormalPosition(position: NormalPosition, movement: Movement): NormalPosition = {
     movement match {
-      case Movement(Direction.Down, num) => (x, y, a + num)
-      case Movement(Direction.Up, num) => (x, y, a - num)
-      case Movement(Direction.Forward, num) => (x + num, y + (a * num), a)
-      case Movement(Direction.Backward, num) => (x - num, y, a)
+      case Movement("down", num) => position.copy(y = position.y + num)
+      case Movement("up", num) => position.copy(y = position.y - num)
+      case Movement("forward", num) => position.copy(x = position.x + num)
     }
   }
-  println(s"Aimed Position: ${aimedCalculatedPosition}")
-  var aimedResult = aimedCalculatedPosition._1 * aimedCalculatedPosition._2
-  println(s"Aimed Result: ${aimedResult}")
-
+  def calculateNextAimedPosition(position: AimedPosition, movement: Movement): AimedPosition = {
+    movement match {
+      case Movement("down", num) => position.copy(a =  position.a + num)
+      case Movement("up", num) => position.copy(a = position.a - num)
+      case Movement("forward", num) => position.copy(x = position.x + num, y = position.y + (position.a * num))
+    }
+  }
 
   def inputToMovement(input: String) = {
     input.split(' ').toList match {
-      case "forward" :: num :: Nil => Movement(Direction.Forward, num.toInt)
-      case "backward" :: num :: Nil => Movement(Direction.Backward, num.toInt)
-      case "up" :: num :: Nil => Movement(Direction.Up, num.toInt)
-      case "down" :: num :: Nil => Movement(Direction.Down, num.toInt)
+      case dir :: num :: Nil => Movement(dir, num.toInt)
     }
   }
 }
